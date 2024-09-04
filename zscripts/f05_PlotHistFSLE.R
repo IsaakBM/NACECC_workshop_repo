59138e5992a5024532363ac8aff5d9e11f5c6be5
IsaakBM <ibrito@conservation.org>
2024-09-04 15:01:31 -0700

    Update f05_PlotHistFSLE.R

--- zscripts/f05_PlotHistFSLE.R
+++ zscripts/f05_PlotHistFSLE.R
@@ -3,17 +3,13 @@
 # NO GUARANTEES THAT CODE IS CORRECT
 # Caveat Emptor!
 
-# geom_density() computes a smooth kernel density estimate (KDE), which is a smoothed version of the histogram. 
-# It gives you a sense of the distribution shape, but it can sometimes obscure or smooth out smaller peaks or variations 
-# if they are not prominent enough.
-
 source("zscripts/z_helpFX.R")
 source("zscripts/f04_MergeClean.R")
 
-dfF <- modelvsrealH(indirData = "outputs_sb/sb_LSP_F", 
-                     indirModel = "outputs_sb/sb_LSP_T")
+dfF <- ForagingTransit(indirFor = "outputs_sb/sb_LSP_F", 
+                       indirTran = "outputs_sb/sb_LSP_T")
 
-gg_dfF <- entropie_mmf(input = dfF, 
-             xlabs = "Distance to high FSLE (km)", 
-             ylabs = "Density")
+gg_dfF <- kernel_ggplot(input = dfF, 
+                        xlabs = "Distance to high FSLE (km)", 
+                        ylabs = "Density")
 ggsave("figures/z_testing/LSP_2021-07.png", plot = gg_dfF, width = 10, height = 6, dpi = 300, limitsize = FALSE)
